6baa3e9ef703a08b392363430294a4dc3f6dae08
assangha <82292317+assangha@users.noreply.github.com>
2022-11-14 22:53:20 -0500

    change text

--- hotel-app/src/App.js
+++ hotel-app/src/App.js
@@ -47,7 +47,7 @@ export default function SignIn() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Login
+            Client Login
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
