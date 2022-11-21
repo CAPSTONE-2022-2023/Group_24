5b7eaba738b97553b0f0311277075d3bd1148162
assangha <82292317+assangha@users.noreply.github.com>
2022-11-21 14:59:20 -0500

    add Home

--- hotel-app/src/employee-home.js
+++ hotel-app/src/employee-home.js
@@ -12,8 +12,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CHome from './client-home';
-import EHome from './employee-home';
+
 const theme = createTheme({
   palette: {
     primary: {
@@ -22,7 +21,7 @@ const theme = createTheme({
   }
 });
 
-export default function SignIn() {
+export default function EHome() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -48,54 +47,9 @@ export default function SignIn() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Client Login
+            Employee Home
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Not a member? Sign Up!"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
+          
         </Box>
       </Container>
     </ThemeProvider>
