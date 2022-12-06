34e83df3cb9b7ec43bfc3b15ab701f48148054ec
The Punslinger <59809715+Pun-slinger@users.noreply.github.com>
2022-12-06 10:05:20 -0500

    minor fixes for room create

--- hotel-app/src/Room-Create.js
+++ hotel-app/src/Room-Create.js
@@ -4,7 +4,6 @@ import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import { MenuItem, FormControl, InputLabel, Select } from '@mui/material';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -12,19 +11,6 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from "axios";
-
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const theme = createTheme();
 
@@ -62,12 +48,8 @@ export default function Room_Create() {
 
     axios.post("http://localhost:3001/room/create", newRoom);
     alert(`Room ${data.get("name")} successful`);
+    navigate('/room/view');
   };
-
-  // const handleSubmitEquipment = (event) => {
-  //   event.preventDefault();
-  //   console.log("Added equipment");
-
 
   // };
   // if (localStorage.getItem("username") === null || localStorage.getItem("username") === "") {
